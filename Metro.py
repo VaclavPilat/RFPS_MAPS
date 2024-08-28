93c6f94b8f723a8cc7bc59a07960d1fffa45ac9b
Václav Pilát <pilatvac@fit.cvut.cz>
2024-08-28 14:58:03 +0200

    Creating outer walls

--- Metro.py
+++ Metro.py
@@ -16,16 +16,25 @@ UNDERPASS_ENTRANCE_STAIRS = 5
 
 def underpass(S: V = V(0, 0, 0)):
     global metro
+    # Outer points
     TRO = S
-    TRI = TRO + UNDERPASS_ENTRANCE_BORDER * (V.LEFT + V.BACKWARD)
     TLO = TRO + UNDERPASS_ENTRANCE_SLOPE * V.LEFT
-    TLI = TLO + UNDERPASS_ENTRANCE_BORDER * V.BACKWARD
     BRO = TRO + UNDERPASS_ENTRANCE_WIDTH * V.BACKWARD
-    BRI = BRO + UNDERPASS_ENTRANCE_BORDER * (V.LEFT + V.FORWARD)
     BLO = BRO + UNDERPASS_ENTRANCE_SLOPE * V.LEFT
+    # Inner points
+    TRI = TRO + UNDERPASS_ENTRANCE_BORDER * (V.LEFT + V.BACKWARD)
+    TLI = TLO + UNDERPASS_ENTRANCE_BORDER * V.BACKWARD
+    BRI = BRO + UNDERPASS_ENTRANCE_BORDER * (V.LEFT + V.FORWARD)
     BLI = BLO + UNDERPASS_ENTRANCE_BORDER * V.FORWARD
-    metro.face([TLO, TRO, BRO, BLO, BLI, BRI, TRI, TLI])
-
+    # Upper points
+    TRO1, TLO1, BRO1, BLO1, TRI1, TLI1, BRI1, BLI1 = (x + UNDERPASS_ENTRANCE_TOP * V.UP for x in (TRO, TLO, BRO, BLO, TRI, TLI, BRI, BLI))
+    metro.face([TLO1, TRO1, BRO1, BLO1, BLI1, BRI1, TRI1, TLI1])
+    # Outer walls
+    metro.face([TLO, TLI, TLI1, TLO1])
+    metro.face([TLO, TRO, TRO1, TLO1])
+    metro.face([TRO, BRO, BRO1, TRO1])
+    metro.face([BLO, BRO, BRO1, BLO1])
+    metro.face([BLO, BLI, BLI1, BLO1])
 
 Scene.clear()
 metro = Model()
