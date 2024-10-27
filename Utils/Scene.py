4bb0e9a4b8a5da283369ae7715422721212c3d66
Václav Pilát <pilatvac@fit.cvut.cz>
2024-10-27 19:14:11 +0100

    Added a Scene file

---
+++ Utils/Scene.py
@@ -0,0 +1,24 @@
+## \file
+# Class for creating a scene or a map
+from Object import Mesh, Container
+
+
+
+class Scene(Container):
+    """Class for a map, a top level object containing meshes
+    """
+    
+    def __iter__(self):
+        """Iterating over meshes
+
+        Returns:
+            Iterator representing mesh objects
+        """
+        for obj in self.objects:
+            yield from obj
+    
+    def create(self) -> None:
+        """Creating the scene by materializing meshes and their faces
+        """
+        for obj in self:
+            print(obj.name)
