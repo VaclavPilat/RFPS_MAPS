338a2df41b3fcf2070939e189bd98d2620923050
Václav Pilát <pilatvac@fit.cvut.cz>
2024-10-27 19:57:26 +0100

    Added a simple Babel file

---
+++ Babel.py
@@ -0,0 +1,35 @@
+## \file
+# Implementation of the Babel map
+from Utils import *
+
+
+
+class Column(Mesh):
+    """Cylinder mesh with vertical walls only
+    """
+
+    def generate(self, height: int|float = 5, radius: int|float = 0.5, segments: int = 8) -> None:
+        """Generating a column
+
+        Args:
+            height (int | float, optional): Column height. Defaults to 5.
+            radius (int | float, optional): Column radius. Defaults to 0.5.
+            segments (int, optional): Number of outer vertical faces. Defaults to 8.
+        """
+        pass
+
+
+
+class Babel(Scene):
+    """Implementation of the Tower of Babel map
+    """
+
+    def generate(self) -> None:
+        """Generating Babel structure
+        """
+        pass
+
+
+
+if __name__ == "__main__":
+    Babel().create()
