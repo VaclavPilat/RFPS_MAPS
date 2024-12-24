46c73d7b906b11b26aaf363faeff884f2810a454
Václav Pilát <pilatvac@fit.cvut.cz>
2024-12-24 16:30:35 +0100

    Replaced reprWrapper subclassing with method injection

--- Utils/Wrapper.py
+++ Utils/Wrapper.py
@@ -4,29 +4,51 @@
 
 
 def reprWrapper(cls: "cls") -> "cls":
-    """Class wrapper that returns a subclass of the wrapped one
-    Adds a __repr__ method that returns a representation of the construction call
+    """Class wrapper that adds __repr__ method.
+    repr(obj) should return a string representation of the constructor call.
 
     Args:
         cls (cls): Class type to wrap
 
     Returns:
-        cls: Subclass type of the wrapped class
+        cls: The same class type with updated members
     """
-    class reprWrapped(cls):
-        """Wrapped class
-        """
-        def __init__(self, *args, **kwargs) -> None:
-            """Initializing the argstring an calling wrapped class constructor
-            """
-            self.argstring = ", ".join([repr(a) for a in args] + [f"{key}={repr(value)}" for (key, value) in kwargs.items()])
-            super().__init__(*args, **kwargs)
-        def __repr__(self) -> str:
-            """Returns string representation of a constructor call
+    old_init = cls.__init__
+    def new_init(self, *args, **kwargs) -> None:
+        self.argstring = ", ".join([repr(a) for a in args] + [f"{key}={repr(value)}" for (key, value) in kwargs.items()])
+        old_init(self, *args, **kwargs)
+    def new_repr(self) -> str:
+        return f"{self.__class__.__name__}({self.argstring})"
+    cls.__init__ = new_init
+    cls.__repr__ = new_repr
+    return cls
 
-            Returns:
-                str: String representing constructor call
+
+
+if __name__ == "__main__":
+    @reprWrapper
+    class Wrapped:
+        """Example class being wrapped
+        """
+        def __init__(self, value: int = 10):
+            """Initialising an optional value
+
+            Args:
+                value (int, optional): Example value. Defaults to 10.
             """
-            name = cls.__name__ if self.__class__ == reprWrapped else self.__class__.__name__
-            return f"{name}({self.argstring})"
-    return reprWrapped
+            self.value = value
+    import unittest
+    class ReprWrapperTest(unittest.TestCase):
+        """Class for testing @reprWrapper implementation
+        """
+        def test_repr(self) -> None:
+            """Testing repr() output
+            """
+            self.assertEqual(repr(Wrapped()), "Wrapped()")
+            self.assertEqual(repr(Wrapped(5)), "Wrapped(5)")
+        def test_fields(self) -> None:
+            """Testing field value
+            """
+            self.assertEqual(Wrapped().value, 10)
+            self.assertEqual(Wrapped(5).value, 5)
+    unittest.main()
