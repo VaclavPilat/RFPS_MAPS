419c49356e4e6ffc0e3504ea49384d0613cb3ceb
Václav Pilát <pilatvac@fit.cvut.cz>
2025-01-05 12:28:11 +0100

    Added tests for simple bounds usage

--- Tests/CircleTest.py
+++ Tests/CircleTest.py
@@ -45,25 +45,41 @@ class CircleTest(unittest.TestCase):
     def test_almost_equal(self) -> None:
         """Testing cases when generated vertices are almost equal
         """
-        self.assertTuplesAlmostEqual(Circle(radius=1, points=1).vertices(), (V3.RIGHT, ))
-        self.assertTuplesAlmostEqual(Circle(radius=1, points=2).vertices(), (V3.RIGHT, V3.LEFT))
-        self.assertTuplesAlmostEqual(Circle(radius=1, points=4).vertices(), (V3.RIGHT, V3.FORWARD, V3.LEFT, V3.BACKWARD))
+        circle = Circle(radius=1)
+        self.assertTuplesAlmostEqual(circle(points=1).vertices(), (V3.RIGHT, ))
+        self.assertTuplesAlmostEqual(circle(points=2).vertices(), (V3.RIGHT, V3.LEFT))
+        self.assertTuplesAlmostEqual(circle(points=4).vertices(), (V3.RIGHT, V3.FORWARD, V3.LEFT, V3.BACKWARD))
         with self.assertRaises(AssertionError):
-            self.assertTuplesAlmostEqual(Circle(radius=1, points=1).vertices(), (V3.LEFT, ))
+            self.assertTuplesAlmostEqual(circle(points=1).vertices(), (V3.LEFT, ))
         with self.assertRaises(AssertionError):
-            self.assertTuplesAlmostEqual(Circle(radius=1, points=2).vertices(), (V3.RIGHT, V3.RIGHT))
+            self.assertTuplesAlmostEqual(circle(points=2).vertices(), (V3.RIGHT, V3.RIGHT))
         with self.assertRaises(AssertionError):
-            self.assertTuplesAlmostEqual(Circle(radius=1, points=4).vertices(), (V3.RIGHT, V3.BACKWARD, V3.LEFT, V3.FORWARD))
+            self.assertTuplesAlmostEqual(circle(points=4).vertices(), (V3.RIGHT, V3.BACKWARD, V3.LEFT, V3.FORWARD))
     
     def test_not_almost_equal(self) -> None:
         """Testing cases when generated vertices are not almost equal
         """
+        circle = Circle(radius=1)
         with self.assertRaises(AssertionError):
-            self.assertTuplesNotAlmostEqual(Circle(radius=1, points=1).vertices(), (V3.RIGHT, ))
+            self.assertTuplesNotAlmostEqual(circle(points=1).vertices(), (V3.RIGHT, ))
         with self.assertRaises(AssertionError):
-            self.assertTuplesNotAlmostEqual(Circle(radius=1, points=2).vertices(), (V3.RIGHT, V3.LEFT))
+            self.assertTuplesNotAlmostEqual(circle(points=2).vertices(), (V3.RIGHT, V3.LEFT))
         with self.assertRaises(AssertionError):
-            self.assertTuplesNotAlmostEqual(Circle(radius=1, points=4).vertices(), (V3.RIGHT, V3.FORWARD, V3.LEFT, V3.BACKWARD))
-        self.assertTuplesNotAlmostEqual(Circle(radius=1, points=1).vertices(), (V3.LEFT, ))
-        self.assertTuplesNotAlmostEqual(Circle(radius=1, points=2).vertices(), (V3.RIGHT, V3.RIGHT))
-        self.assertTuplesNotAlmostEqual(Circle(radius=1, points=4).vertices(), (V3.RIGHT, V3.BACKWARD, V3.LEFT, V3.FORWARD))
+            self.assertTuplesNotAlmostEqual(circle(points=4).vertices(), (V3.RIGHT, V3.FORWARD, V3.LEFT, V3.BACKWARD))
+        self.assertTuplesNotAlmostEqual(circle(points=1).vertices(), (V3.LEFT, ))
+        self.assertTuplesNotAlmostEqual(circle(points=2).vertices(), (V3.RIGHT, V3.RIGHT))
+        self.assertTuplesNotAlmostEqual(circle(points=4).vertices(), (V3.RIGHT, V3.BACKWARD, V3.LEFT, V3.FORWARD))
+    
+    def test_bounds(self) -> None:
+        """Testing vertices generated within bounds
+        """
+        circle = Circle(radius=1, points=4)
+        self.assertTuplesAlmostEqual(circle(bounds=(0, 360)).vertices(), (V3.RIGHT, V3.FORWARD, V3.LEFT, V3.BACKWARD, V3.RIGHT, ))
+        self.assertTuplesAlmostEqual(circle(bounds=(0, 300)).vertices(), (V3.RIGHT, V3.FORWARD, V3.LEFT, V3.BACKWARD, ))
+        self.assertTuplesAlmostEqual(circle(bounds=(0, 270)).vertices(), (V3.RIGHT, V3.FORWARD, V3.LEFT, V3.BACKWARD, ))
+        self.assertTuplesAlmostEqual(circle(bounds=(0, 200)).vertices(), (V3.RIGHT, V3.FORWARD, V3.LEFT, ))
+        self.assertTuplesAlmostEqual(circle(bounds=(0, 180)).vertices(), (V3.RIGHT, V3.FORWARD, V3.LEFT, ))
+        self.assertTuplesAlmostEqual(circle(bounds=(0, 150)).vertices(), (V3.RIGHT, V3.FORWARD, ))
+        self.assertTuplesAlmostEqual(circle(bounds=(0, 90)).vertices(), (V3.RIGHT, V3.FORWARD, ))
+        self.assertTuplesAlmostEqual(circle(bounds=(0, 60)).vertices(), (V3.RIGHT, ))
+        self.assertTuplesAlmostEqual(circle(bounds=(0, 0)).vertices(), (V3.RIGHT, ))
