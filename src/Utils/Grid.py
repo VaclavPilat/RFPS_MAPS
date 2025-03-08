2d8003d6ca39a34b0a4adec3ae5cc7553a015a2a
Václav Pilát <pilatvac@fit.cvut.cz>
2025-03-09 00:00:09 +0100

    Added a color legend to Grid

--- src/Utils/Grid.py
+++ src/Utils/Grid.py
@@ -5,7 +5,18 @@
 
 class Grid:
     """Functionality for rendering Object vertices in console from multiple perspectives
+
+    Requires a subclass to have a "faces" property (a list of lists of V3 vertices)
     """
+
+    ## Grid colors
+    # ANSI colors, from coldest to hottest
+    GRID_COLORS = ("\033[37m", "\033[36m", "\033[34m", "\033[32m", "\033[33m", "\033[31m", "\033[35m")
+
+    def gridLegend(self) -> None:
+        """Printing out a legend for grid colors
+        """
+        print(" ".join(self.GRID_COLORS[i] + str(i) for i in range(len(self.GRID_COLORS))), end="+\033[0m\n")
     
     def gridAxis(self, axis: str, reversed: bool = False) -> tuple:
         values = sorted(set(round(getattr(vertex, axis), 3) for face in self.faces for vertex in face))
@@ -51,11 +62,20 @@ class Grid:
                 print(str(y).ljust(Yjust)[i], end="")
             print()
     
-    def printGrids(self) -> None:
-        """Printing out a string representation of an object in a grid view
+    def printGrids(self, top: bool = True, side: bool = True, front: bool = True) -> None:
+        """Printing out a string representations of an object in a grid view
+
+        Args:
+            top (bool, optional): Render a top view? Defaults to True.
+            side (bool, optional): Render a side view? Defaults to True.
+            front (bool, optional): Render a front view? Defaults to True.
         """
         if not self.faces:
             return
-        self.printGrid(*self.gridAxis("x"), *self.gridAxis("y", True))
-        self.printGrid(*self.gridAxis("z", True), *self.gridAxis("y", True))
-        self.printGrid(*self.gridAxis("z", True), *self.gridAxis("x"))
+        self.gridLegend()
+        if top:
+            self.printGrid(*self.gridAxis("x"), *self.gridAxis("y", True))
+        if side:
+            self.printGrid(*self.gridAxis("z", True), *self.gridAxis("y", True))
+        if front:
+            self.printGrid(*self.gridAxis("z", True), *self.gridAxis("x"))
