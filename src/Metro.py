846b872cc514a1fd3557d270b0ea064ec09dcbc5
Václav Pilát <pilatvac@fit.cvut.cz>
2025-03-10 21:01:11 +0100

    Added incomplete slope generation

--- src/Metro.py
+++ src/Metro.py
@@ -96,40 +96,59 @@ class Tile(Object):
 
 @createObjectSubclass(Tile)
 def Stairs(self, D: float, G: int = 3, H: float = 0.2, L: float = 0.3) -> None:
-    """Generating a flight of stairs with multiple possible step groups
+    """Generating multiple flights of stairs with resting places in between
 
     Args:
-        D (float): Total staircase depth
+        D (float): Total staircase depth (in meters)
         G (int, optional): Number of step groups. Defaults to 3.
-        H (float, optional): Step height (will be adjusted). Defaults to 0.2.
-        L (float, optional): Step length. Defaults to 0.3.
+        H (float, optional): Step height (in meters, will be adjusted). Defaults to 0.2.
+        L (float, optional): Step length (in meters). Defaults to 0.3.
     """
     assert G >= 1, "At least 1 step group required"
     VF = round(D / H) # Vertical face count
     HF = VF + 1 # Horizontal face count
-    assert HF * L < abs(self.TR - self.TL), "Steps will not fit horizontally"
+    assert HF * L < self.width, "Steps will not fit horizontally"
     TL, BL = (self.TL, self.BL)
     if G >= 2:
-        R = (abs(self.TR - self.TL) - (HF - G + 1) * L) / (G - 1) # Resting place length
+        R = (self.width - (HF - G + 1) * L) / (G - 1) # Resting place length
     I = set(int(i / G * HF) for i in range(1, G)) # Resting place indices
     for i in range(HF):
         if i == HF - 1: # Final horizontal face
             self.face(self.TR(z=TL.z), TL, BL, self.BR(z=BL.z))
             break
         # Making a horizontal face
-        TL1, BL1 = map(lambda x: x + V3.RIGHT * (R if i in I else L), (TL, BL))
+        TL1, BL1 = map(lambda v: v + V3.RIGHT * (R if i in I else L), (TL, BL))
         self.face(TL1, TL, BL, BL1)
         # Making a vertical face
         z = (self.TL + V3.DOWN * D * (i + 1) / VF).z
-        TL2, BL2 = map(lambda x: x(z=z), (TL1, BL1))
+        TL2, BL2 = map(lambda v: v(z=z), (TL1, BL1))
         self.face(TL2, TL1, BL1, BL2)
         TL, BL = (TL2, BL2)
 
 
 
 @createObjectSubclass(Tile)
-def Slopes(self, D: float, S: int = 3) -> None:
-    self.face(self.TR, self.TL, self.BL, self.BR)
+def Slopes(self, D: float, S: int = 3, R: float = 8) -> None:
+    """Generating multiple (wheelchair accessible) slopes with resting places in between
+
+    Args:
+        D (float): Total depth (in meters)
+        S (int, optional): Slope count. Defaults to 3.
+        R (float, optional): Slope ratio (slope length per meter of descent). Defaults to 8.
+    """
+    assert S >= 1, "At least 1 slope is required"
+    assert D * R < self.width, "Slopes would not fit horizontally"
+    G = (self.width - D * R) / (S - 1) # Resting place (gap) length
+    TL, BL = (self.TL, self.BL)
+    for i in range(S):
+        # Making a slope face
+        z = (self.TL + V3.DOWN * D * (i + 1) / S).z
+        TL1, BL1 = map(lambda v: (v + V3.RIGHT * (TL.z - z) * R)(z=z), (TL, BL))
+        self.face(TL1, TL, BL, BL1)
+        # Making a horizontal face
+        TL2, BL2 = map(lambda v: v + V3.RIGHT * G, (TL1, BL1))
+        self.face(TL2, TL1, BL1, BL2)
+        TL, BL = (TL2, BL2)
 
 
 
@@ -137,8 +156,8 @@ def Slopes(self, D: float, S: int = 3) -> None:
 def Metro(self) -> None:
     """Generating the Metro station
     """
-    self.load(Stairs, "Underpass entrance stairs", V3.ZERO, 10, 3, D=4).printGrids()
-    self.load(Slopes, "Underpass entrance slopes", V3.BACKWARD * 3, 15, 3, D=4).printGrids()
+    self.load(Stairs, "Underpass entrance stairs", V3.ZERO, 10, 3, D=4)
+    self.load(Slopes, "Underpass entrance slopes", V3.BACKWARD * 3, 40, 3, D=4).printGrids()
 
 
 
