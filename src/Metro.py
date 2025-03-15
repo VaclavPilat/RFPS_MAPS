749fd9d8a926c3e6e1800e791399efd8b0824f55
Václav Pilát <pilatvac@fit.cvut.cz>
2025-03-15 12:49:41 +0100

    Added classes for representing Tile boundaries

--- src/Metro.py
+++ src/Metro.py
@@ -28,16 +28,109 @@ class Pivot(Enum):
     """
 
     ## Top left corner
-    TOP_LEFT = 0
+    TL = 0
 
     ## Top right corner
-    TOP_RIGHT = 1
+    TR = 1
 
     ## Bottom left corner
-    BOTTOM_LEFT = 2
+    BL = 2
 
     ## Bottom right corner
-    BOTTOM_RIGHT = 3
+    BR = 3
+
+
+
+@makeImmutable
+class Bounds:
+    """Class for representing Tile bounds
+    """
+
+    def __init__(self, O: V3, TL: V3, BR: V3, R: int = 0) -> None:
+        """Initialising Tile boundaries
+
+        Args:
+            O (V3): Tile origin (pivot) point
+            TL (V3): Top left tile corner
+            BR (V3): Bottom right tile corner
+            R (int, optional): Rotation index. Defaults to 0.
+        """
+        ## Tile origin (pivot) point
+        self.O = O
+        ## Top left tile corner
+        assert TL.z == BR.z, "All bounds have to have the same z-value"
+        self.TL = TL
+        ## Top right tile corner
+        self.TR = TL(y=BR.y)
+        ## Bottom left tile corner
+        self.BL = TL(x=BR.x)
+        ## Bottom right tile corner
+        self.BR = BR
+        ## Tile rotation index
+        self.R = R
+        ## Tile width
+        self.W = abs(self.TL - self.TR)
+        assert self.W > 0, "Tile width has to be positive"
+        ## Tile height
+        self.H = abs(self.TL - self.BL)
+        assert self.H > 0, "Tile height has to be positive"
+
+    def rotate(self, point: V3) -> V3:
+        """Rotating a vertex point around tile pivot
+
+        Args:
+            point (V3): Point to rotate
+
+        Returns:
+            V3: Rotated vertex position
+        """
+        return self.O + ((point - self.O) >> self.R)
+
+
+
+@addInitRepr
+class Box(Bounds):
+    """Class for representing a bounding box of a Tile
+    """
+
+    def __init__(self, O: V3, W: float, H: float, P: Pivot = Pivot.TL, R: int = 0) -> None:
+        """Initialising a bounding box
+
+        Args:
+            O (V3): Tile origin (pivot) point
+            W (float): Tile width
+            H (float): Tile height
+            P (Pivot, optional): Pivot location. Defaults to Pivot.TL.
+            R (int, optional): Rotation index. Defaults to 0.
+        """
+        if P == Pivot.TL:
+            TL = O
+            BR = TL + V3.RIGHT * W + V3.BACKWARD * H
+        else:
+            raise ValueError("Unexpected Pivot value")
+        super().__init__(O, TL, BR, R)
+
+
+
+@addInitRepr
+class Anchor(Bounds):
+    """Class for representing anchor bounds of a Tile
+    """
+
+    def __init__(self, TL: V3, BR: V3, P: Pivot = Pivot.TL, R: int = 0) -> None:
+        """Initialising anchor bounds
+
+        Args:
+            TL (V3): Top left tile corner
+            BR (V3): Bottom right tile corner
+            P (Pivot, optional): Pivot location. Defaults to Pivot.TL.
+            R (int, optional): Rotation index. Defaults to 0.
+        """
+        if P == Pivot.TL:
+            O = TL
+        else:
+            raise ValueError("Unexpected Pivot value")
+        super().__init__(O, TL, BR, R)
 
 
 
@@ -46,50 +139,21 @@ class Tile(Object):
     """Base class for all Metro Objects
     """
 
-    def __init__(self, name: str, position: V3, width: float, height: float, pivot: Pivot = Pivot.TOP_LEFT, rotation: int = 0, *args, **kwargs) -> None:
+    def __init__(self, name: str, bounds: Bounds, *args, **kwargs) -> None:
         """Initialising a Metro tile
 
         Args:
             name (str): Tile name
-            position (V3): Tile position
-            width (float): Tile width
-            height (float): tile height
-            pivot (Pivot, optional): Pivot point. Defaults to Pivot.TOP_LEFT.
-            rotation (int, optional): Rotation index. Defaults to 0.
+            bounds (Bounds): Tile boundaries
         """
-        ## Tile width
-        self.width = width
-        ## Tile height
-        self.height = height
-        ## Pivot point
-        self.pivot = pivot
-        ## Rotation index
-        self.rotation = rotation
-        # Calculating bounds
-        if self.pivot == Pivot.TOP_LEFT:
-            self.TL = position
-            self.TR = position + V3.RIGHT * width
-            self.BL = position + V3.BACKWARD * height
-            self.BR = self.BL + V3.RIGHT * width
-        else:
-            raise ValueError("Unexpected Pivot value")
-        super().__init__(name, position, *args, **kwargs)
-    
-    def rotate(self, point: V3) -> V3:
-        """Rotating a point around tile pivot
-
-        Args:
-            point (V3): Point to rotate
-
-        Returns:
-            V3: Rotated point
-        """
-        return ((point - self.position) >> self.rotation) + self.position
+        ## Tile boundaries
+        self.bounds = bounds
+        super().__init__(name, self.bounds.O, *args, **kwargs)
     
     def face(self, *points, **kwargs) -> None:
         """Rotating face around tile pivot
         """
-        points = list(map(self.rotate, points))
+        points = list(map(self.bounds.rotate, points))
         super().face(*points, **kwargs)
 
 
@@ -107,20 +171,20 @@ def Stairs(self, D: float, G: int = 3, H: float = 0.2, L: float = 0.3) -> None:
     assert G >= 1, "At least 1 step group required"
     VF = round(D / H) # Vertical face count
     HF = VF + 1 # Horizontal face count
-    assert HF * L < self.width, "Steps will not fit horizontally"
-    TL, BL = (self.TL, self.BL)
+    assert HF * L < self.bounds.W, "Steps will not fit horizontally"
+    TL, BL = (self.bounds.TL, self.bounds.BL)
     if G >= 2:
-        R = (self.width - (HF - G + 1) * L) / (G - 1) # Resting place length
+        R = (self.bounds.W - (HF - G + 1) * L) / (G - 1) # Resting place length
     I = set(int(i / G * HF) for i in range(1, G)) # Resting place indices
     for i in range(HF):
         if i == HF - 1: # Final horizontal face
-            self.face(self.TR(z=TL.z), TL, BL, self.BR(z=BL.z))
+            self.face(self.bounds.TR(z=TL.z), TL, BL, self.bounds.BR(z=BL.z))
             break
         # Making a horizontal face
         TL1, BL1 = map(lambda v: v + V3.RIGHT * (R if i in I else L), (TL, BL))
         self.face(TL1, TL, BL, BL1)
         # Making a vertical face
-        z = (self.TL + V3.DOWN * D * (i + 1) / VF).z
+        z = (self.bounds.TL + V3.DOWN * D * (i + 1) / VF).z
         TL2, BL2 = map(lambda v: v(z=z), (TL1, BL1))
         self.face(TL2, TL1, BL1, BL2)
         TL, BL = (TL2, BL2)
@@ -137,17 +201,17 @@ def Slopes(self, D: float, S: int = 3, R: float = 8) -> None:
         R (float, optional): Slope ratio (slope length per meter of descent). Defaults to 8.
     """
     assert S >= 1, "At least 1 slope is required"
-    assert D * R < self.width, "Slopes would not fit horizontally"
+    assert D * R < self.bounds.W, "Slopes would not fit horizontally"
     if S > 1:
-        G = (self.width - D * R) / (S - 1) # Resting place (gap) length
-    TL, BL = (self.TL, self.BL)
+        G = (self.bounds.W - D * R) / (S - 1) # Resting place (gap) length
+    TL, BL = (self.bounds.TL, self.bounds.BL)
     C = 1 if S == 1 else S - 1 # Resting place count
     for i in range(S + C):
         if i > 0 and i == S + C - 1: # Final face, whatever it might be
-            self.face(TL, BL, *map(lambda v: v + V3.DOWN * D, (self.BR, self.TR)))
+            self.face(TL, BL, *map(lambda v: v + V3.DOWN * D, (self.bounds.BR, self.bounds.TR)))
             break
         if i % 2 == 0: # Making a slope face
-            z = (self.TL + V3.DOWN * D * (i // 2 + 1) / S).z
+            z = (self.bounds.TL + V3.DOWN * D * (i // 2 + 1) / S).z
             TL1, BL1 = map(lambda v: (v + V3.RIGHT * (TL.z - z) * R)(z=z), (TL, BL))
         else: # Making a horizontal face
             TL1, BL1 = map(lambda v: v + V3.RIGHT * G, (TL, BL))
@@ -164,18 +228,18 @@ def UnderpassEntrance(self, H: float = 0.1, W: float = 0.3) -> None:
         H (float, optional): Curb height (in meters). Defaults to 0.1.
         W (float, optional): Curb width (in meters). Defaults to 0.3.
     """
-    TLI, TRI = map(lambda v: v + V3.BACKWARD * W, (self.TL, self.TR))
-    BLI, BRI = map(lambda v: v + V3.FORWARD * W, (self.BL, self.BR))
+    TLI, TRI = map(lambda v: v + V3.BACKWARD * W, (self.bounds.TL, self.bounds.TR))
+    BLI, BRI = map(lambda v: v + V3.FORWARD * W, (self.bounds.BL, self.bounds.BR))
     TRI, BRI = map(lambda v: v + V3.LEFT * W, (TRI, BRI))
-    TL1, TR1, BL1, BR1 = map(lambda v: v + V3.UP * H, (self.TL, self.TR, self.BL, self.BR))
+    TL1, TR1, BL1, BR1 = map(lambda v: v + V3.UP * H, (self.bounds.TL, self.bounds.TR, self.bounds.BL, self.bounds.BR))
     TLI1, TRI1, BLI1, BRI1 = map(lambda v: v + V3.UP * H, (TLI, TRI, BLI, BRI))
     self.face(TR1, TL1, TLI1, TRI1, BRI1, BLI1, BL1, BR1) # Top face
     # Outer faces
-    self.face(TR1, BR1, self.BR, self.TR)
-    self.face(TL1, TR1, self.TR, self.TL)
-    self.face(BR1, BL1, self.BL, self.BR)
-    self.face(TLI1, TL1, self.TL, TLI)
-    self.face(BL1, BLI1, BLI, self.BL)
+    self.face(TR1, BR1, self.bounds.BR, self.bounds.TR)
+    self.face(TL1, TR1, self.bounds.TR, self.bounds.TL)
+    self.face(BR1, BL1, self.bounds.BL, self.bounds.BR)
+    self.face(TLI1, TL1, self.bounds.TL, TLI)
+    self.face(BL1, BLI1, BLI, self.bounds.BL)
     # Inner faces
     self.face(BLI1, BRI1, BRI, BLI)
     self.face(BRI1, TRI1, TRI, BRI)
@@ -187,9 +251,9 @@ def UnderpassEntrance(self, H: float = 0.1, W: float = 0.3) -> None:
 def Metro(self) -> None:
     """Generating the Metro station
     """
-    #self.load(Stairs, "Underpass entrance stairs", V3.ZERO, 10, 3, D=4)
-    #self.load(Slopes, "Underpass entrance slopes", V3.BACKWARD * 3, 40, 3, D=4)
-    self.load(UnderpassEntrance, "Underpass entrance", V3.ZERO, 10, 3).printGrids()
+    self.load(Stairs, "Underpass entrance stairs", Box(V3.ZERO, 10, 3), D=4)
+    self.load(Slopes, "Underpass entrance slopes", Box(V3.BACKWARD * 3, 40, 3), D=4)
+    self.load(UnderpassEntrance, "Underpass entrance", Box(V3.ZERO, 10, 3)).printGrids()
 
 
 
