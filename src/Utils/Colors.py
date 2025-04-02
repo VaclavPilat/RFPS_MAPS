21f3f4687f2ab8ab94985ba7d70582198afc5ac7
Václav Pilát <pilatvac@fit.cvut.cz>
2025-04-02 14:36:38 +0200

    Turning off colors if stdout is not a console

--- src/Utils/Colors.py
+++ src/Utils/Colors.py
@@ -1,19 +1,23 @@
 ## \file
 # Variables with ANSI color strings
+import sys
 
 
 
-## Reset color string
-NONE = "\033[0m"
-
-## Grid colors, sorted from coldest to hottest
-TEMPERATURE = ("\033[94;1m", "\033[96;1m", "\033[92;1m", "\033[93;1m", "\033[91;1m", "\033[95;1m")
-
-## Axis colors
-AXIS = {"x": "\033[91;1m", "y": "\033[92;1m", "z": "\033[94;1m"}
-
-## Hierarchy colors
-HIERARCHY = ("\033[94m", "\033[95m", "\033[96m", "\033[91m", "\033[92m", "\033[93m")
+if sys.stdout.isatty():
+    ## Reset color string
+    NONE = "\033[0m"
+    ## Grid colors, sorted from coldest to hottest
+    TEMPERATURE = ("\033[94;1m", "\033[96;1m", "\033[92;1m", "\033[93;1m", "\033[91;1m", "\033[95;1m")
+    ## Axis colors
+    AXIS = {"x": "\033[91;1m", "y": "\033[92;1m", "z": "\033[94;1m"}
+    ## Hierarchy colors
+    HIERARCHY = ("\033[94m", "\033[95m", "\033[96m", "\033[91m", "\033[92m", "\033[93m")
+else:
+    NONE = ""
+    TEMPERATURE = ("", )
+    AXIS = {"x": "", "y": "", "z": ""}
+    HIERARCHY = ("", )
 
 
     
