3e3c6ba607f90181aefcec14b971cfbbb5dad287
Václav Pilát <pilatvac@fit.cvut.cz>
2025-04-21 13:44:25 +0200

    Added a basic Line implementation

---
+++ src/Utils/Line.py
@@ -0,0 +1,50 @@
+## \file
+# Class for representing a line between two points
+from .Decorators import makeImmutable, addInitRepr
+from .Vector import V3
+
+
+@makeImmutable
+@addInitRepr
+class Line:
+    """A line connecting two points.
+    """
+
+    def __init__(self, a: V3, b: V3) -> None:
+        """Initialize a line.
+
+        Args:
+            a (V3): First point of the line.
+            b (V3): Second point of the line.
+        """
+        if a == b:
+            raise ValueError("Points must be different")
+        self.a = a
+        self.b = b
+
+    def __eq__(self, other: "Line") -> bool:
+        """Checking line equality
+
+        Returns:
+            bool: True if the line is equal to the other line.
+        """
+        if not isinstance(other, Line):
+            return False
+        return (self.a == other.a and self.b == other.b) or (self.a == other.b and self.b == other.a)
+
+    def __hash__(self) -> int:
+        """Hashing a line instance.
+
+        Returns:
+            int: The hash value of the instance.
+        """
+        return hash(frozenset((self.a, self.b)))
+
+    def __call__(self, *args, **kwargs) -> "Line":
+        """Making a copy of a line.
+
+        Returns:
+            Line: A copy of the line with altered params.
+        """
+        # noinspection PyCallingNonCallable
+        return Line(self.a(*args, **kwargs), self.b(*args, **kwargs))
---
+++ tests/LineTest.py
@@ -0,0 +1,41 @@
+## \file
+# Line implementation tests
+from src.Utils.Line import Line
+from src.Utils.Vector import V3
+import unittest
+
+
+class TestLine(unittest.TestCase):
+    """Class for testing Line implementation.
+    """
+
+    def test_constructor(self) -> None:
+        """Testing Line constructor
+        """
+        Line(V3.ZERO, V3.ONE)
+        with self.assertRaises(ValueError):
+            Line(V3.ZERO, V3.ZERO)
+
+    def test_call(self) -> None:
+        """Testing Line copy call
+        """
+        with self.assertRaises(ValueError):
+            Line(V3.ONE, V3.FORWARD)(0, 0, 0)
+        self.assertEqual(Line(V3.ONE, V3.FORWARD)(), Line(V3.ONE, V3.FORWARD))
+        self.assertEqual(Line(V3.ONE, V3.FORWARD)(x=0), Line(V3.LEFT + V3.UP, V3.ZERO))
+
+    def test_equality(self) -> None:
+        """Testing Line equality
+        """
+        self.assertEqual(Line(V3.ZERO, V3.ONE), Line(V3.ZERO, V3.ONE))
+        self.assertEqual(Line(V3.ZERO, V3.ONE), Line(V3.ONE, V3.ZERO))
+        self.assertNotEqual(Line(V3.ZERO, V3.ONE), Line(V3.FORWARD, V3.ONE))
+        self.assertNotEqual(Line(V3.ZERO, V3.ONE), Line(V3.ZERO, V3.FORWARD))
+
+    def test_hash(self) -> None:
+        """Testing Line hashing
+        """
+        self.assertFalse(Line(V3.ZERO, V3.ONE) in set())
+        self.assertTrue(Line(V3.ZERO, V3.ONE) in {Line(V3.ZERO, V3.ONE), })
+        self.assertTrue(Line(V3.ONE, V3.ZERO) in {Line(V3.ZERO, V3.ONE), })
+        self.assertFalse(Line(V3.ONE, V3.ZERO) in {Line(V3.ZERO, V3.FORWARD), })
